8f869a909445e3f86f9026d8e3df58a043ccc3de
mike <mike@peakffice.com>
2026-01-08 20:04:16 +0900

    Initial commit

---
+++ src/main/java/com/ss_shot/ss_shot_be/SsShotBeApplication.java
@@ -0,0 +1,13 @@
+package com.ss_shot.ss_shot_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsShotBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsShotBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ss_shot/ss_shot_be/SsShotBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss_shot.ss_shot_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsShotBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
